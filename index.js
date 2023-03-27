93cfbcc506efa478e6012d480ed8f7870f0be85b
Thailer <thailer.palmer@gmail.com>
2023-03-27 19:22:08 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function isPalindrome(word) {
+    // iterate from the beginning of the string to the middle of the string
+    for (let i = 0; i < word.length / 2; i++) {
+      // compare the letter we're iterating over to the corresponding letter at the end of the string
+      const j = word.length - 1 - i;
+      if (word[i] !== word[j]) {
+        // if the letters don't match, return false
+        return false;
+      }
+    }
+  
+    // if we reach the middle, and all the letters match, return true
+    return true;
+  }
   // Write your algorithm here
-}
 
 /* 
   Add your pseudocode here
